b414de919cc982a0299d72f07379e3f05b9a1c8d
姜科 <bazzjiang@hotmail.com>
2023-02-01 21:48:52 +0800

    添加demo

--- src/main/kotlin/com/jiangkedev/file/FileRouters.kt
+++ src/main/kotlin/com/jiangkedev/file/FileRouters.kt
@@ -1,9 +1,12 @@
 package com.jiangkedev.file
 
+import com.jiangkedev.endWithJson
+import com.jiangkedev.file.dto.AttachmentInfoDto
 import com.jiangkedev.request
 import com.jiangkedev.response
 import io.vertx.core.AsyncResult
 import io.vertx.core.Vertx
+import io.vertx.core.json.JsonObject
 import io.vertx.ext.web.Router
 import io.vertx.mysqlclient.MySQLPool
 import io.vertx.sqlclient.Row
@@ -17,6 +20,9 @@ import io.vertx.sqlclient.SqlConnection
  */
 fun fileRouters(vertx: Vertx,pool : MySQLPool?):Router{
   return Router.router(vertx).apply {
+    /**
+     * 根据id查询附件信息
+     */
     get("/file/getInfoById/:id").handler { ctx ->
       val id = ctx.request.getParam("id")
       pool?.connection?.compose { conn: SqlConnection ->
@@ -27,6 +33,9 @@ fun fileRouters(vertx: Vertx,pool : MySQLPool?):Router{
             if (ar.succeeded()) {
               //打印查询结果
               ar.result().forEach { row -> run { println(row.getString("attach_name")) } }
+              val result : MutableList<JsonObject> = ArrayList();
+              ar.result().forEach { row -> run { result.add(row.toJson()) } }
+              ctx.response.endWithJson(result)
             } else {
               println("Something went wrong " + ar.cause().message)
             }
---
+++ src/main/kotlin/com/jiangkedev/file/dto/AttachmentInfoDto.kt
@@ -0,0 +1,19 @@
+package com.jiangkedev.file.dto
+
+/**
+ *@author 姜科 <bazzjiang@hotmail.com>
+ *@date 2023-02-01
+ *@description 附件信息
+ */
+data class AttachmentInfoDto(
+
+  /**
+   * id
+   */
+  val id:Long,
+
+  /**
+   * 附件名称
+   */
+  val attachName:String
+)
