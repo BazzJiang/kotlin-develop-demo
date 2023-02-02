e9add1e2d815c30425045c6b622de31436fa5b79
姜科 <bazzjiang@hotmail.com>
2023-02-02 09:09:23 +0800

    优化函数写法

--- src/main/kotlin/com/jiangkedev/file/FileRouters.kt
+++ src/main/kotlin/com/jiangkedev/file/FileRouters.kt
@@ -25,21 +25,23 @@ fun fileRouters(vertx: Vertx,pool : MySQLPool?):Router{
      */
     get("/file/getInfoById/:id").handler { ctx ->
       val id = ctx.request.getParam("id")
-      pool?.connection?.compose { conn: SqlConnection ->
-        conn
-          .query("SELECT * FROM common_attachment where id = ${id}")
-          .execute()
-          .onComplete { ar: AsyncResult<RowSet<Row>> ->
-            if (ar.succeeded()) {
-              //打印查询结果
-              ar.result().forEach { row -> run { println(row.getString("attach_name")) } }
-              val result : MutableList<JsonObject> = ArrayList();
-              ar.result().forEach { row -> run { result.add(row.toJson()) } }
-              ctx.response.endWithJson(result)
-            } else {
-              println("Something went wrong " + ar.cause().message)
+      pool?.let{
+        it.connection.compose { conn: SqlConnection ->
+          conn
+            .query("SELECT * FROM common_attachment where id = ${id}")
+            .execute()
+            .onComplete { ar: AsyncResult<RowSet<Row>> ->
+              if (ar.succeeded()) {
+                //打印查询结果
+                ar.result().forEach { row -> run { println(row.getString("attach_name")) } }
+                val result : MutableList<JsonObject> = ArrayList();
+                ar.result().forEach { row -> run { result.add(row.toJson()) } }
+                ctx.response.endWithJson(result)
+              } else {
+                println("Something went wrong " + ar.cause().message)
+              }
             }
-          }
+        }
       }
     }
   }
